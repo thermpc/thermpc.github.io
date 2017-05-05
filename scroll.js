c30782a335625d71cb8f08c2c330d2facdbfc665
naveenr414 <dsfan414@gmail.com>
2017-05-05 12:29:30 -0400

    Created menu toggle

--- scroll.js
+++ scroll.js
@@ -23,3 +23,15 @@ $(document).ready(function(){
     } 
   });
 });
+
+function toggleMenu() 
+{
+  var menuBox = document.getElementById('menu');    
+  if(menuBox.style.display == "block") {
+    menuBox.style.display = "none";
+  }
+  else
+  { 
+    menuBox.style.display = "block";
+  }
+}
