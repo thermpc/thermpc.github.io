8b71f39f4030f1f489aa9454ae963969ff3279a7
naveenr414 <dsfan414@gmail.com>
2017-05-05 09:18:04 -0400

    Created Scrolling Javascript

---
+++ scroll.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") 
+    {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        }, 
+        800, 
+        function()
+        {
+          window.location.hash = hash;
+        }
+      );
+    } 
+  });
+});
