7987698520b38657f331573578bdc08c83238d87
Elvin <whyu.tilt@gmail.com>
2017-05-14 19:44:16 -0400

    Re-edit some values

--- scroll.js
+++ scroll.js
@@ -69,7 +69,7 @@ $(window).scroll(function (event)
 function scrollPosition()
 {
     var scroll = $(window).scrollTop();
-    if(scroll<=800)
+    if(scroll<=600)
     {
       $("#introButton").addClass("filled");
     }
@@ -79,7 +79,7 @@ function scrollPosition()
       $("#introButton").removeClass("filled");
     }
 
-    if(scroll>800 && scroll<=1600)
+    if(scroll>600 && scroll<=1600)
     {
       $("#aboutButton").addClass("filled");
     }
@@ -88,7 +88,7 @@ function scrollPosition()
       $("#aboutButton").removeClass("filled");
     }
 
-    if(scroll>1600 && scroll<=2400)
+    if(scroll>1600 && scroll<=2600)
     {
       $("#sampleButton").addClass("filled");
     }
@@ -97,7 +97,7 @@ function scrollPosition()
       $("#sampleButton").removeClass("filled");
     }
 
-    if(scroll>2400)
+    if(scroll>2600)
     {
       $("#contactButton").addClass("filled");
     }
