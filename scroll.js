f0a24318594afafd81c2ed21db633ff0879b288d
Elvin <whyu.tilt@gmail.com>
2017-05-14 19:34:35 -0400

    Change some scroll values (test)

--- scroll.js
+++ scroll.js
@@ -68,8 +68,8 @@ $(window).scroll(function (event)
 
 function scrollPosition()
 {
-        var scroll = $(window).scrollTop();
-    if(scroll<=350)
+    var scroll = $(window).scrollTop();
+    if(scroll<=800)
     {
       $("#introButton").addClass("filled");
     }
@@ -79,7 +79,7 @@ function scrollPosition()
       $("#introButton").removeClass("filled");
     }
 
-    if(scroll>350 && scroll<=1050)
+    if(scroll>800 && scroll<=1600)
     {
       $("#aboutButton").addClass("filled");
     }
@@ -88,7 +88,7 @@ function scrollPosition()
       $("#aboutButton").removeClass("filled");
     }
 
-    if(scroll>1050 && scroll<=1650)
+    if(scroll>1600 && scroll<=2400)
     {
       $("#sampleButton").addClass("filled");
     }
@@ -97,7 +97,7 @@ function scrollPosition()
       $("#sampleButton").removeClass("filled");
     }
 
-    if(scroll>1650)
+    if(scroll>2400)
     {
       $("#contactButton").addClass("filled");
     }
