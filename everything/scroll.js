162e4bdfd48d3f65c05db1c093850b7590e940e7
vanillascrewdriver <psychfan99@gmail.com>
2017-06-21 12:51:58 -0400

    Update scroll.js
    Fixed scroll so that it doesn't cover title of section

--- everything/scroll.js
+++ everything/scroll.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate(
         {
-          scrollTop: $(hash).offset().top
+          scrollTop: $(hash).offset().top-50
         }, 
         800, 
         function()
